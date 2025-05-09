0dbfb1c0f452896132c0a8ad7d476b60888c6cf6
ctitus <ctitus@umd.edu>
2025-05-08 23:52:24 -0400

    Cam info working, please future me, make this a docker container

--- run-udp-video-publisher.sh
+++ run-udp-video-publisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /opt/ros/humble/setup.bash && source ~/IdeaProjects/px4-ros2-localization/ws_sensor_combined/install/local_setup.bash
+source /opt/ros/humble/setup.bash # && source ~/IdeaProjects/px4-ros2-localization/ws_sensor_combined/install/local_setup.bash
 
 ros2 run udp_video_pkg udp_video_publisher
--- ws_sensor_combined/src/udp_video_pkg/udp_video_pkg/udp_video_publisher.py
+++ ws_sensor_combined/src/udp_video_pkg/udp_video_pkg/udp_video_publisher.py
@@ -1,17 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
 import cv2
+import yaml
 
 
 class UDPVideoPublisher(Node):
     def __init__(self):
         super().__init__('udp_video_publisher')
-        self.publisher_ = self.create_publisher(Image, 'udp_video/image_raw', 10)
+
+        self.publisher_image = self.create_publisher(Image, 'udp_video/image_raw', 10)
+        self.publisher_info = self.create_publisher(CameraInfo, 'udp_video/camera_info', 10)
+
         self.bridge = CvBridge()
 
-        # GStreamer pipeline to receive UDP stream (change port if needed)
+        # Load YAML manually using PyYAML
+        self.camera_info = self.load_camera_info_from_yaml("/home/ctitus/.ros/camera_info/gz500_camera.yaml")
+
+        # GStreamer pipeline to receive UDP stream
         self.pipeline = (
             'udpsrc port=5600 ! application/x-rtp, encoding-name=H264, payload=96 ! '
             'rtph264depay ! avdec_h264 ! videoconvert ! appsink'
@@ -22,14 +29,39 @@ class UDPVideoPublisher(Node):
             self.get_logger().error('Failed to open video stream!')
             return
 
-        timer_period = 0.033  # ~30 FPS
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.timer = self.create_timer(1.0 / 30.0, self.timer_callback)  # ~30 FPS
+
+    def load_camera_info_from_yaml(self, path):
+        with open(path, 'r') as f:
+            calib = yaml.safe_load(f)
+
+        cam_info = CameraInfo()
+        cam_info.width = calib['image_width']
+        cam_info.height = calib['image_height']
+        cam_info.distortion_model = calib['distortion_model']
+        cam_info.d = calib['distortion_coefficients']['data']
+        cam_info.k = calib['camera_matrix']['data']
+        cam_info.r = calib['rectification_matrix']['data']
+        cam_info.p = calib['projection_matrix']['data']
+        cam_info.binning_x = 0
+        cam_info.binning_y = 0
+        cam_info.roi.x_offset = 0
+        cam_info.roi.y_offset = 0
+        cam_info.roi.height = 0
+        cam_info.roi.width = 0
+        cam_info.roi.do_rectify = False
+        return cam_info
 
     def timer_callback(self):
         ret, frame = self.cap.read()
         if ret:
             msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
-            self.publisher_.publish(msg)
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = 'camera_link'
+
+            self.camera_info.header = msg.header
+            self.publisher_image.publish(msg)
+            self.publisher_info.publish(self.camera_info)
         else:
             self.get_logger().warn('Failed to read frame')
 
